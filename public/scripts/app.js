6bc537e4c9ff322c4260a53b57b7585887357d6c
Maliha Hossain <malu.hoss@gmail.com>
2019-03-22 00:15:25 +0000

    fixed formatting issues on new tweet and posted tweets, also added error messages to slide up and down for text area

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,63 +8,21 @@
 
 $(document).ready(function() {
   $('textarea').focus();
-  $('textarea').focus(function() {
-      $("#ring-girl").show();
-      setTimeout(function() { $("#ring-girl").hide(); }, 2500);
-    })
-  // .blur(function() {
-  //     $("#ring-girl").hide()
-  //   });
 
-     // $('textarea').focus();
+  //FOR A FUN TIME. COMMENT THIS BACK IN//
+
+  // $('textarea').focus(function() {
+  //     $("#ring-girl").show();
+  //     setTimeout(function() { $("#ring-girl").hide(); }, 1000);
+  //   })
+
+
+  //    $('textarea').focus();
 
       $('#ring-girl').hide();
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
+
+
+
   function escape(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -120,8 +78,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     if ($(this).find('textarea').val().length === 0 || $(this).find('textarea').val().length > 140) {
-      alert("Error! Messages need to have text and be less than 140 characters. Please modify your message.");
+      $(".c-validation").slideDown();
+      $(".c-validation").text("Error! Messages need to have text and be less than 140 characters. Please modify your message.");
     } else {
+      $(".c-validation").hide(350);
       $.ajax({
         type: "POST",
         url: '/tweets',
